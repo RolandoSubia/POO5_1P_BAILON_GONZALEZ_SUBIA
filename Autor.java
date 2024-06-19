77a877747d20e9b6213ce17587e0ee373e02c66e
RolandoSubia <rolansub@gmail.com>
2024-06-19 08:25:59 -0500

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -1,1 +1,31 @@
+public class Autor{
+  private String institucion;
+  private String campo;
+  private String usuario;
+  private String contrasenia;
+  private static char rol;
+  
+    public Autor{
+      this.institucion = institucion;
+      this.campo = campo;
+      this.usuario = usuario;
+      this.contrasenia = contrasenia;
+      rol = 'A';
+    } 
+    
+    public void ingresarDatosAutor(String usuario, String nuevaContrasenia, String nuevaInstitucion, String nuevoCampo){
+      this.user = usuario;
+      this.contrasenia = nuevaContrasenia;
+      this.institucion = nuevaInstitucion;
+      this.campo = nuevoCampo;
+    }
+
+    //public void registrarDatosAutor(String usuario, String nuevaContrasenia, String nuevaInstitucion, String nuevoCampo){
+    //}
+    
+    public boolean someterRevision(){
+      return true;
+    }
+    
+    
 
