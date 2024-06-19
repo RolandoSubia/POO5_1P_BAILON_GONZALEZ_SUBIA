99c50ed4d9f73df6360fadb5daf21a8394344cad
Charles Bailon <111033802+CharlesBailon2@users.noreply.github.com>
2024-06-19 00:35:22 -0500

    Create Articulo.java

---
+++ Articulo.java
@@ -0,0 +1,61 @@
+
+package trabajoautonomo4;
+
+public class Articulo {
+    private String titulo;
+    private String codigo;
+    private String resumen;
+    private String contenido;
+    private String palabrasClave;
+
+    public Articulo() {
+    }
+
+    public Articulo(String titulo, String codigo, String resumen, String contenido, String palabrasClave) {
+        this.titulo = titulo;
+        this.codigo = codigo;
+        this.resumen = resumen;
+        this.contenido = contenido;
+        this.palabrasClave = palabrasClave;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getPalabrasClave() {
+        return palabrasClave;
+    }
+
+    public void setPalabrasClave(String palabrasClave) {
+        this.palabrasClave = palabrasClave;
+    }
+}
