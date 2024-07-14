670a9dfa2f23f569291a1c8682190d3446fde797
RolandoSubia <rolansub@gmail.com>
2024-07-14 12:12:42 -0500

    Update Revisor.java

--- Revisor.java
+++ Revisor.java
@@ -1,46 +1,77 @@
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
-import java.util.Scanner;
+
+/**
+ * 
+ * @author Grupo3
+ * 
+ * 
+ */
 
 public class Revisor extends Usuario {
+    //Atributos especificos para la clase Revisor
     private String especialidad;
     private int numArticulosRevisados;
     private boolean tieneArticuloPendiente = false;
     private Articulo articuloAsignado;
 
-    // Constructor para inicializar un objeto Revisor
+    /**
+     * Constructor para inicializar un objeto Revisor.
+     *@param usuario Nombre de Usuario
+     *@param contrasenia Contraseña de Usuario
+     *@param nombre Nombre del revisor
+     *@param apellido Apellido del revisor
+     *@param email Email del revisor
+     *@param especialidad Especialidad del revisor
+     **/
     public Revisor(String usuario, String contrasenia, String nombre, String apellido, String email, String especialidad) {
         super(usuario, contrasenia, nombre, apellido, email);
         this.especialidad = especialidad;
         this.numArticulosRevisados = 0; 
     }
 
+    /**
+    *Sirve para devolver la especialidad.
+    *@return Devuelve la especialidad, un string. 
+    **/
     public String getEspecialidad() {
         return especialidad;
     }
 
+    /**
+    *Establece la especialidad.
+    *@param especialidad. Especialidad del revisor
+    **/
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
     }
 
+    /**
+    *Sirve para devolver el numero de articulos revisados por el revisor. 
+    *@return Devuelve el numero de articulos revisados por el revisor. 
+    **/
     public int getNumArticulosRevisados() {
         return numArticulosRevisados;
     }
 
+    /**
+    * Incrementa el numero de articulos revisados.
+    **/
     public void incrementarArticulosRevisados() {
         this.numArticulosRevisados++;
     }
 
+    /**
+    *Genera un correo para el revisor. 
+    **/
     @Override
     public void generarCorreo() {
         System.out.println("Correo generado para el Revisor: " + getEmail());
     }
 
-    //Metodo si decide aceptar o no el articulo
+    /**
+    *Metodo que decide si recibir el articulo o no.
+    *@param sc  Scanner para recibir la orden
+    *@param revisiones. Lista que contienen las revisiones
+    **/
     @Override
     public void decidirArticulo(Scanner sc, List<Revision> revisiones) {
         if (tieneArticuloPendiente) {
@@ -68,7 +99,11 @@ public class Revisor extends Usuario {
         }
     }
 
-    // Método específico del revisor para gestionar la revisión
+    /**
+    *Metodo específico del revisor para gestionar la revisión.
+    *@param sc Scanner para recibir la orden
+    *@param revisiones. Lista que contiene las revisiones
+    **/ 
     public void gestionarRevisionRevisor(Scanner sc, List<Revision> revisiones) {
         System.out.println("Articulos asignados para revision");
         for (Revision rev : revisiones) {
@@ -101,6 +136,11 @@ public class Revisor extends Usuario {
         }
     }
 
+    /**
+    *Metodo específico para guardar los detalles de revision.
+    *@param revision. Metodo que se invoca para guardar la revision.
+    *@return Nada.
+    **/ 
     private void guardarRevision(Revision revis) {
         try {
             String data3 = String.join(",", revis.getCodigoArticulo(), revis.getRevisorId(), revis.getComentarios(), String.valueOf(revis.isDecision()));
@@ -110,19 +150,29 @@ public class Revisor extends Usuario {
             }
             Files.write(Paths.get("revisiones.txt"), (data3 + System.lineSeparator()).getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
-            System.err.println("Error al guardar la revision: " + e.getMessage());
+            System.err.println("Error al guardar la revision");
         }
     }
 
+    /**
+    *Metodo específico para enviar el articulo al editor.
+    *@param revision. Metodo que se invoca para enviar la revision
+    *@return Nada.
+    **/ 
     private void enviarArticuloAlEditor(Revision revision) {
         // Implementar la lógica para enviar el artículo al editor
         System.out.println("El articulo ha sido enviado al editor para su revision final.");
     }
     
+    /**
+    *Metodo para asignar un articulo al revisor.
+    *@param articulo. Metodo que se invoca para asignar el articulo.
+    **/ 
     public void asignarArticulo(Articulo articulo) {
         this.articuloAsignado = articulo;
         this.tieneArticuloPendiente = true;
     }
 }
+
 
 
